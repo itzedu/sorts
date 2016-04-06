e7fa48432b75ce4fe48d1fb1932a07644379c14e
Eduardo <edubaik@gmail.com>
2016-04-06 09:28:30 -0700

    added comments to insertion sort

--- insertion.js
+++ insertion.js
@@ -1,26 +1,23 @@
-function insertionSort(array) {
+function insertionSort(arr) {
+  var length = arr.length;
 
-  var length = array.length;
+  for (var i = 1; i < length; i++) { // outer loop represents the unsorted portion of the array
+    var element = arr[i]; // element to be inserted
+    var index = i; // represent where the element is going to be inserted to
 
-  for (var i = 1; i < length; i++) {
-    var element = array[i];
-    var index = i;
-
-    for (var j = i - 1; j > -1; j--) {
-      if (array[j] > element) {
-        array[j+1] = array[j];
+    for (var j = i - 1; j > -1; j--) { // innner loop represents the sorted portion of the array
+      if (arr[j] > element) {
+        arr[j+1] = arr[j];
         index = j;
       } else {
         break;
       }
     }
-    array[index] = element;
+    arr[index] = element;
   }
-  return array;
+  return arr;
 }
 
-var random = [];
-for (var i = 0; i < 100; i++) {
-  random.push(Math.floor(Math.random()*100)+1);
-}
-console.log(insertionSort(random));
+console.log(insertionSort([79, 74, 25, 14, 85, 35, 81, 69, 7, 83, 2]));
+console.log(insertionSort([1,2,3,4,5,6,7,8,9]));
+console.log(insertionSort([9,8,7,6,5,4,3,2,1]));
